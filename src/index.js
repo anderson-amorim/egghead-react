691f0e6272748cd70f4336ad3ffe96a4936b32b5
anderson <anderson.gw2@gmail.com>
2017-05-17 12:49:50 -0300

    Use map to Create React Components from Arrays of Data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import App2 from './App2';
+import App3 from './App3';
 
 ReactDOM.render(
-  <App txt="I've come to talk with you again" cat={5}/>, document.getElementById('root'));
+  <App txt="I've come to talk with you again" cat={5} />, document.getElementById('root'));
 ReactDOM.render(
   <App2 />, document.getElementById('root2'));
+ReactDOM.render(
+  <App3 />, document.getElementById('root3'));
