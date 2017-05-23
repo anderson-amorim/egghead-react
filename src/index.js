f0c84d699d3e11fb13caeaae77f545e9e8d05c6c
anderson <anderson.gw2@gmail.com>
2017-05-23 12:59:32 -0300

    Write More Reusable React Components with Composable APIs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App3 from './App3';
 import App4 from './App4';
 import App5 from './App5';
 import App6 from './App6';
+import App7 from './App7';
 
 ReactDOM.render(
   <App txt="I've come to talk with you again" cat={5} />, document.getElementById('root'));
@@ -19,3 +20,5 @@ ReactDOM.render(
   <App5 />, document.getElementById('root5'));
 ReactDOM.render(
   <App6 />, document.getElementById('root6'));
+ReactDOM.render(
+  <App7 />, document.getElementById('root7'));
