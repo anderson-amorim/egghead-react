8e36c27c15ab93cb02625fc6ad061c062b2cfb29
anderson <anderson.gw2@gmail.com>
2017-06-02 12:43:59 -0300

    Redux: Implementing Store from Scratch

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,10 +4,32 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import CounterReducer from './counter.reducer';
-import { createStore } from 'redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        }
+    };
+
+    dispatch({});
+
+    return { getState, dispatch, subscribe };
+}
 
 const reducer = new CounterReducer();
 const store = createStore(reducer.counter);
