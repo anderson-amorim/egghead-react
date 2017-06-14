611f3de56b7c35d26b8f13e644c270891fd7e77a
anderson <anderson.gw2@gmail.com>
2017-06-14 11:47:19 -0300

    react

--- redux/src/index.js
+++ redux/src/index.js
@@ -5,40 +5,6 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import CounterReducer from './counter.reducer';
 import { createStore } from 'redux';
+import TodoApp from './todos.reducer.js';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-
-const Counter = ({ value, onIncrement, onDecrement }) => (
-    <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-    </div>
-);
-
-const reducer = new CounterReducer();
-const store = createStore(reducer.counter);
-
-const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={store.getState()}
-            onIncrement={() => {
-                store.dispatch({
-                    type: 'INCREMENT'
-                })
-            }}
-            onDecrement={() => {
-                store.dispatch({
-                    type: 'DECREMENT'
-                })
-            }}
-        />
-        , document.getElementById('root')
-    );
-}
-
-store.subscribe(render);
-render();
